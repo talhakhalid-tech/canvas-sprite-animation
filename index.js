4ab76296b43bcb81caedd3db00c2a5543344a8a3
talhakhalid-tech <talhakhalid0323@gmail.com>
2020-08-30 17:12:48 +0500

    made character run

--- index.js
+++ index.js
@@ -7,52 +7,53 @@ window.onload = function () {
 
   const img = new Image();
   img.src = "/tile.png";
+  img.onload = () => {
+    var cellWidth = 192;
+    var cellHeight = 200;
 
-  var cellWidth = 192;
-  var cellHeight = 200;
+    drawImage = (horIndex, verIndex, count) => {
+      context.drawImage(
+        img,
+        horIndex * cellWidth,
+        verIndex * cellHeight,
+        cellWidth,
+        cellHeight,
+        // 350,
+        (canvas.width / 8) * count,
+        75,
+        cellWidth,
+        cellHeight
+      );
+    };
 
-  drawImage = (horIndex, verIndex, count) => {
-    context.drawImage(
-      img,
-      horIndex * cellWidth,
-      verIndex * cellHeight,
-      cellWidth,
-      cellHeight,
-      // 350,
-      (canvas.width / 8) * count,
-      75,
-      cellWidth,
-      cellHeight
-    );
-  };
+    let horCell = 0;
+    let verCell = 0;
+    let count = 0;
+    let date = new Date();
 
-  let horCell = 0;
-  let verCell = 0;
-  let count = 0;
-  let date = new Date();
+    animateFunction = () => {
+      let now = new Date();
+      if (now - date >= 150) {
+        date = now;
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-  animateFunction = () => {
-    let now = new Date();
-    if (now - date >= 150) {
-      date = now;
-      context.clearRect(0, 0, canvas.width, canvas.height);
-
-      console.log(count);
-      horCell++;
-      count++;
-      if (horCell % 4 === 0) {
-        horCell = 0;
-        verCell++;
-        if (verCell % 2 === 0) {
-          verCell = 0;
-          count = 0;
+        console.log(count);
+        horCell++;
+        count++;
+        if (horCell % 4 === 0) {
+          horCell = 0;
+          verCell++;
+          if (verCell % 2 === 0) {
+            verCell = 0;
+            count = 0;
+          }
         }
+        drawImage(horCell, verCell, count);
       }
-      drawImage(horCell, verCell, count);
-    }
+
+      window.requestAnimationFrame(animateFunction);
+    };
 
     window.requestAnimationFrame(animateFunction);
   };
-
-  window.requestAnimationFrame(animateFunction);
 };
