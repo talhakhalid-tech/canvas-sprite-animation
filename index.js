2bd18d1032715ac774b05ffcfbbd191c80b7505b
talhakhalid-tech <talhakhalid0323@gmail.com>
2020-08-30 13:19:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+  //definitions
+  const canvas = document.querySelector("canvas");
+  canvas.width = 900;
+  canvas.height = 450;
+  const context = canvas.getContext("2d");
+
+  const img = new Image();
+  img.src = "tile.png";
+
+  var cellWidth = 192;
+  var cellHeight = 200;
+
+  img.onload = () => {
+    // context.drawImage(
+    //   img,
+    //   0,
+    //   0,
+    //   cellWidth,
+    //   cellHeight,
+    //   0,
+    //   0,
+    //   cellWidth,
+    //   cellHeight
+    // );
+  };
+
+  drawImage = (horIndex, verIndex) => {
+    context.drawImage(
+      img,
+      horIndex * cellWidth,
+      verIndex * cellHeight,
+      cellWidth,
+      cellHeight,
+      350,
+      75,
+      cellWidth,
+      cellHeight
+    );
+  };
+
+  let horCell = 0;
+  let verCell = 0;
+  let date = new Date();
+
+  animateFunction = () => {
+    let now = new Date();
+    if (now - date >= 100) {
+      date = now;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      horCell++;
+      if (horCell % 4 === 0) {
+        horCell = 0;
+        verCell++;
+        if (verCell % 2 === 0) {
+          verCell = 0;
+        }
+      }
+      drawImage(horCell, verCell);
+    }
+
+    window.requestAnimationFrame(animateFunction);
+  };
+
+  window.requestAnimationFrame(animateFunction);
+};
